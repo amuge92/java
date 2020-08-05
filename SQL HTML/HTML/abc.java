6c88390a635e72f6388af4d0b4dd4e867c9a5de8
amuge92 <osyee@naver.com>
2020-08-05 18:22:07 +0900

    new file

---
+++ SQL HTML/HTML/abc.java
@@ -0,0 +1,12 @@
+public class abc{
+
+    public static void main(String[] args) {
+        System.out.println("s");
+        double a ;
+        double b;
+        a= 101010101010101;
+        b= 101010101010101;
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
